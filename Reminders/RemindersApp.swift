5025d38c30b4fa11510cdca6f93199f87a37829a
Denys Zaiakin <denis.zayakin@trackimo.com>
2024-02-18 17:07:39 +0200

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Denys Zaiakin on 18.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
